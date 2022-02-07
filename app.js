cc82fcb4eaeb45676f5667d9d00a1ddb1d86437f
Salome Gomez <sgomez713@misena.edu.co>
2022-02-06 22:01:28 -0500

    se hizo la logica para los click events si se presiona un cuadro con bomba, si se presiona un cuadro vacio y que al presionar un cuadro vacio se presionen los cuadros alrededor de este

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let width = 10
     let bombAmount = 20
     let squares = []
+    let isGameOver = false
 
     // Create board
     function createBoard() {
@@ -24,6 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
             square.classList.add(shuffledArray[i])
             grid.appendChild(square)
             squares.push(square)
+
+            // normal click
+            square.addEventListener('click', function(e) {
+                click(square)
+            })
         }
 
         // add numbers to the squares
@@ -49,5 +55,82 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     createBoard()
+
+    // click on square actions
+    function click(square) {
+        let currentId = square.id
+        // what happens when is Game Over
+        if (isGameOver) return
+        if (square.classList.contains('checked') || square.classList.contains('flag')) return
+        // squares with bombs
+        if (square.classList.contains('bomb')) {
+            console.log('Game Over');
+        } else {
+            // normal squares close to bombs
+            let total = square.getAttribute('data')
+            if (total !=0) {
+                square.classList.add('checked')
+                square.innerHTML = total
+                return
+            }
+            checkSquare(square, currentId)
+        }
+        // normal squares not close to bombs
+        square.classList.add('checked')
+    }
+
+    // check neighboring squares once square is clicked
+    function checkSquare(square, currentId) {
+        // checking all squares around the checked square
+
+        // verify if the checked square is in the right o left edge
+        const isLeftEdge = (currentId % width === 0)
+        const isRightEdge = (currentId % width === width - 1)
+
+        // checking all so around squares
+        setTimeout(() => {
+            if (currentId > 0 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) -1].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId > 9 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) +1 -width].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId > 10) {
+                const newId = squares[parseInt(currentId -width)].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId > 11 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) -1 -width].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId < 98 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) +1].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId < 90 && !isLeftEdge) {
+                const newId = squares[parseInt(currentId) -1 +width].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId < 88 && !isRightEdge) {
+                const newId = squares[parseInt(currentId) +1 +width].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+            if (currentId < 89) {
+                const newId = squares[parseInt(currentId) +width].id
+                const newSquare = document.getElementById(newId)
+                click(newSquare)
+            }
+        }, 10)
+
+    }
 
 })
